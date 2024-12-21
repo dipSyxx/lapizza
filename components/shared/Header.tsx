facb80e8fec94c1b6233896dd4624691ae7d07ed
Yura <linetsky.yura@gmail.com>
2024-12-21 19:40:23 +0100

    init

---
+++ components/shared/Header.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+interface Props {
+  hasSearch?: boolean;
+  hasCart?: boolean;
+  className?: string;
+}
+
+export const Header: React.FC<Props> = ({ className }) => {
+  return <div className={className}>Header</div>;
+};
